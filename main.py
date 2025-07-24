7608fb4a45fc4767a404f2b05a2a1ff668ccc4cd
Anh Khoa <anhkhoa512005@gmail.com>
2025-07-24 09:27:29 +0700

    change overview title

--- main.py
+++ main.py
@@ -46,10 +46,10 @@ st.markdown("""
 
 # Define page navigation
 pages = [
-    st.Page("pages/overview/overview.py", title="Overview"),
     st.Page("pages/live_cam/live_cam.py", title="Live cam"),
     st.Page("pages/control/control.py", title="Control"),
     st.Page("pages/statistic/statistic.py", title="Statistic"),
+    st.Page("pages/overview/overview.py", title="About us"),
 ]
 
 # If logged in, show navigation; else show login/register
